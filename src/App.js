133c0d1dca317922f34ebbca732cd5ba52d869b0
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2020-08-23 19:40:21 +0300

    lesson-1: added base structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './App.module.scss';
 
 function App() {
-  return <div className={classes.App} />;
+  return <div className={classes.App}>aaa</div>;
 }
 
 export default App;
