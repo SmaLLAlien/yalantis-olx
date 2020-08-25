adcf5beb33e478b91c6b057bae016ca8617f5355
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2020-08-25 11:45:45 +0300

    lesson-1: added 404 component

--- src/components/AppRoutes/AppRoutes.js
+++ src/components/AppRoutes/AppRoutes.js
@@ -4,15 +4,18 @@ import { Routes } from '../../global/constants';
 import Products from '../../features/products/containers/Products/Products';
 import FullProduct from '../../features/products/containers/FullProduct/FullProduct';
 import Basket from '../../features/products/containers/Basket/Basket';
+import NotFound from "../NotFound/NotFound";
 
 const AppRoutes = () => (
   <Switch>
     <Route path={Routes.PRODUCTS} exact component={Products} />
     <Route path={`${Routes.PRODUCTS}/:id`} exact component={FullProduct} />
     <Route path={Routes.BASKET} exact component={Basket} />
+
     <Route exact path="/">
       <Redirect to={Routes.PRODUCTS} />
     </Route>
+    <Route path='*' component={NotFound} />
   </Switch>
 );
 
