c0df7ce769439a7f3d79ab09bcc0493b4bce9a70
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2020-08-29 14:33:30 +0300

    lesson-1: renamed axios instance

--- src/core/api.js
+++ src/core/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const productInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 });
 
-export default instance;
+export default productInstance;
