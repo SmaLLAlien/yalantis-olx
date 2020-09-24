6ae51c4d708ed239392c92b5bef0f55992a856d2
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2020-09-24 17:33:35 +0300

    lesson-4: added toastr

--- src/containers/MainLayout/MainLayout.js
+++ src/containers/MainLayout/MainLayout.js
@@ -2,6 +2,12 @@ import React from "react";
 import {productAuthInstanceApi, productInstanceApi} from "../../core/api";
 import Spinner from "../../components/Spinner/Spinner";
 import PropTypes from 'prop-types';
+import {toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import {ERROR_TOASTER_ID, METHODS_FOR_TOASTER, SUCCESS_TOASTER_ID, TOASTER_CONFIG} from "../../global/constants";
+
+toast.configure(TOASTER_CONFIG);
+
 
 export const MainLayout = props => {
   const {children, showSpinner, hideSpinner, isRequested} = props;
@@ -10,12 +16,22 @@ export const MainLayout = props => {
     return config
   }
 
+  const showToaster = (response) => {
+    const method = response.config.method;
+    if (METHODS_FOR_TOASTER.includes(method)) {
+      toast.success('Success!', {toastId: SUCCESS_TOASTER_ID});
+    }
+  }
+
   const successResponse = (response) => {
     hideSpinner();
+    showToaster(response);
     return response;
   }
 
   const error = (error) => {
+    hideSpinner();
+    toast.error(`${error.message}`, {toastId: ERROR_TOASTER_ID});
     return Promise.reject(error);
   }
 
@@ -29,6 +45,7 @@ export const MainLayout = props => {
 
   return (
     <>
+      {/*<ToastContainer limit={1} />*/}
       {isRequested ? <Spinner/> : null},
       {children}
     </>
